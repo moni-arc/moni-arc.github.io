aa5243deb4e2443e02ee4254d335ce97fd84925b
David J. Felix <felix.davidj@gmail.com>
2018-05-11 10:03:49 -0400

    Add base gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `The First Start to Monica's site`,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
+  ],
+}
