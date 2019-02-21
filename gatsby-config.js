2d6061547b01b0e4897414d409a3ca7f9f66aa6b
David J. Felix <felix.davidj@gmail.com>
2019-02-20 22:24:16 -0500

    feat update all of the files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `The First Start to Monica's site`,
+    title: `Monica's site`,
+    description: "This is Monica's Art site",
   },
   plugins: [
     'gatsby-plugin-typescript',
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
     'gatsby-plugin-netlify-cms',
   ],
 }
